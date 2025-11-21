cd4255e03ddd4f7a9e5052a21d4b46023b819c39
unknown <sbganapa1@gmail.com>
2025-11-21 15:12:16 +0530

    initial commit

---
+++ day2_2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int* p = &a;
+    int* p2 = &b;
+    printf("address of a :%p\n",p);
+    printf("address of b :%p\n",p2);
+    printf("%d",*p);
+    return 0;
+}
